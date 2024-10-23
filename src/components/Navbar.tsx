dac1154dbc325bffdd60521e42f4b225d26caa44
Nitu Kumari <79151737+Ni2-1911@users.noreply.github.com>
2024-10-23 18:49:24 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,33 @@
-import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/router";
 
-export default function Navbar() {
+const Navbar = () => {
+  const router = useRouter();
+  const currentPath = router.pathname;
+
+  const navItems = [
+    { name: "Home", path: "/" },
+    { name: "Following", path: "/following" },
+    { name: "Contact Us", path: "/contact-us" },
+  ];
+
   return (
-    <nav className="nav-bar mt-4">
+    <nav className="nav-bar mt-3">
       <ul className="flex">
-        <li className="m-2 page-link">
-          <Link href="/">Home</Link>
-        </li>
-        <li className="m-2 page-link">
-          <Link href="/following">Following</Link>
-        </li>
-        <li className="m-2 page-link">
-          <Link href="/contact-us">Contact Us</Link>
-        </li>
+        {navItems.map((item) => (
+          <li
+            key={item.path}
+            className={`m-2 page-link ${
+              currentPath === item.path ? "border-b-2 border-gray-500" : ""
+            }`}
+            onClick={() => router.push(item.path)}
+          >
+            {item.name}
+          </li>
+        ))}
       </ul>
     </nav>
   );
-}
+};
+
+export default Navbar;
